56358adacabf6b12648b0779bf5b4741939a0c11
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-04-08 19:47:25 +0300

    Add Test and fixed compilation bugs

--- src/Archiver.java
+++ src/Archiver.java
@@ -1,4 +1,7 @@
+import jdk.jshell.spi.ExecutionControl;
+
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -7,62 +10,61 @@ import java.util.Scanner;
 
 public class Archiver {
 
-	public static class Tree()
-	{
-		Node[] nodes;
+    public static class Tree {
+        Node[] nodes;
 
-		public static int getLeavesCount() {
-			throw new NotImplementedException();
-		}
-	}
+        public static int getLeavesCount() {
+            throw new UnsupportedOperationException();
+        }
+    }
 
-	public static class Node {
-	}
+    public static class Node {
+    }
 
-	public static String readLine() {
-		Scanner sc = new Scanner(new File("input.txt"));
-		String s = sc.nextLine();
-		sc.close();
-		return s;
-	}
+    public static String readLine() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        String s = sc.nextLine();
+        sc.close();
+        return s;
+    }
 
-	public static HashMap<Character, Integer> convertStringToFrequencies(String s) { // в частоты встречаемости
-		throw new NotImplementedException();
-	}
+    public static HashMap<Character, Integer> convertStringToFrequencies(String s) { // в частоты встречаемости
+        throw new UnsupportedOperationException();
+    }
 
-	public static Tree convertFrequenciesToTree(HashMap<Character, boolean[]> frequencies) {
-		throw new NotImplementedException();
-	}
+    public static Tree convertFrequenciesToTree(HashMap<Character, Integer> frequencies) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static HashMap<Character, boolean[]> convertTree(Tree v) {
-		throw new NotImplementedException();
-	}
+    public static HashMap<Character, boolean[]> convertTree(Tree v) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static ArrayList<Boolean[]> compressString(String s, HashMap<Character, boolean[]> table) {
-		throw new NotImplementedException();
-	}
+    public static ArrayList<Boolean[]> compressString(String s, HashMap<Character, boolean[]> table) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static ArrayList<Boolean[]> serializeTable(HashMap<Character, boolean[]>) {
-		throw new NotImplementedException();
-	}
+    public static ArrayList<Boolean[]> serializeTable(HashMap<Character, boolean[]> table) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static ArrayList<Boolean[]> concatArrayList(ArrayList<Boolean[]> a, ArrayList<Boolean[]> b) {
-		throw new NotImplementedException();
-	}
+    public static ArrayList<Boolean[]> concatArrayList(ArrayList<Boolean[]> a, ArrayList<Boolean[]> b) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static String convertCodeToString(ArrayList<Boolean[]> code) {
-		throw new NotImplementedException();
-	}
+    public static String convertCodeToString(ArrayList<Boolean[]> code) {
+        throw new UnsupportedOperationException();
+    }
 
-	public static void main(String[] args) {
-		String input = readLine();
-		HashMap<Character, boolean[]> frequencies = convertStringToFrequencies();
-		Tree tree = convertFrequenciesToTree(frequencies);
-		HashMap<Character, boolean[]> table = convertTree(tree);
-		ArrayList<Boolean[]> codeString = convertStrToCode(input, table);
-		ArrayList<Boolean[]> codeTable = tableToCode(table);
-		PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
-		pw.print(convertCodeToString(concatArrayList(codeString, codeTable)));
-		pw.close();
-	}
+    public static void main(String[] args) throws FileNotFoundException {
+        String input = readLine();
+        HashMap<Character, Integer> frequencies = convertStringToFrequencies(input);
+        Tree tree = convertFrequenciesToTree(frequencies);
+        HashMap<Character, boolean[]> table = convertTree(tree);
+        ArrayList<Boolean[]> codeString = compressString(input, table);
+        ArrayList<Boolean[]> codeTable = serializeTable(table);
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+        pw.print(convertCodeToString(concatArrayList(codeString, codeTable)));
+        pw.close();
+    }
 }
---
+++ src/ArchiverTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArchiverTest {
+
+    @org.junit.jupiter.api.Test
+    void convertStringToFrequencies() {
+        assertEquals(2, Archiver.convertCodeToString());
+    }
+}
