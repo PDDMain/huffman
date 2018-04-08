eae529ab223ea57af2e5e85fda05e42cf6c50217
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-04-08 20:09:59 +0300

    Add new test

--- src/ArchiverTest.java
+++ src/ArchiverTest.java
@@ -1,9 +1,14 @@
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArchiverTest {
 
     @org.junit.jupiter.api.Test
     void convertStringToFrequencies() {
-        assertEquals(2, Archiver.convertCodeToString());
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        frequencies.put('p', 5);
+        frequencies.put('d', 2);
+        assertEquals(frequencies, Archiver.convertStringToFrequencies("pppppdd"));
     }
 }
