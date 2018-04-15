eded557a67dc65f80d7c22fe74371d9a6d638f34
PDDMain <37112091+PDDMain@users.noreply.github.com>
2018-04-15 15:09:34 +0300

    new tests for convertStringToFrequencies()

--- src/ArchiverTest.java
+++ src/ArchiverTest.java
@@ -11,4 +11,39 @@ class ArchiverTest {
         frequencies.put('d', 2);
         assertEquals(frequencies, Archiver.convertStringToFrequencies("pppppdd"));
     }
+
+    void convertStringToFrequencies() {
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        frequencies.put(' ', 1);
+        assertEquals(frequencies, Archiver.convertStringToFrequencies(" "));
+    }
+
+    void convertStringToFrequencies() {
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        assertEquals(frequencies, Archiver.convertStringToFrequencies(""));
+    }
+
+    void convertStringToFrequencies() {
+        HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
+        frequencies.put('H', 1);
+        frequencies.put('e', 3);
+        frequencies.put('l', 4);
+        frequencies.put('o', 2);
+        frequencies.put(' ', 5);
+        frequencies.put('w', 1);
+        frequencies.put('r', 1);
+        frequencies.put('d', 1);
+        frequencies.put('!', 1);
+        frequencies.put('M', 1);
+        frequencies.put('y', 1);
+        frequencies.put('n', 2);
+        frequencies.put('a', 2);
+        frequencies.put('m', 3);
+        frequencies.put('i', 2);
+        frequencies.put('s', 1);
+        frequencies.put('D', 1);
+        frequencies.put('e', 3);
+        frequencies.put('.', 1);
+        assertEquals(frequencies, Archiver.convertStringToFrequencies("Hello world! My name is Daniel."));
+    }
 }
